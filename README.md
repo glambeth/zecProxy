10c3c92fb7f1106195341ec446020ec11ec3fc4e
george <glambeth1594@gmail.com>
2017-01-06 15:37:46 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# zecProxy
-Zcash Stratum Proxy
+#Description 
+
+This is a Stratum Proxy for Zcash using JSON-RPC written in Python Twisted. 
+Originally developed for Flypool http://zcash.flypool.org/ 
+
+**WARNING** This is still in active development. Please report any problems you encounter!
+
+#How it Works
+```
+   Pool A <---+                        +-------------+ Rig1 / PC1
+ (Active)      |                       |
+               |                       +-------------+ Rig2 / PC2
+               |                       |
+  Pool B <---+-----StratumProxy  <-----+-------------+ Rig3 / PC3
+(FailOver)                             |
+                                       +-------------+ Rig4 / PC4
+                                      
+```
+
+#Todo
+* implement multiple rig support
+* implement failover 
+* Clean up code
+
+#Requirements
+zecProxy is build with Python 2.7. The requirements for running zecProxy are:
+* Python 2.7+
+* python-twisted
+* linux
+
+#Installation and Start
+* sudo apt-get install python-twisted
+* python ./zecProxy
---
+++ StratumProxy.py
@@ -0,0 +1,146 @@
+from twisted.internet.protocol import Factory
+from twisted.internet import reactor, protocol
+import json
+import re
+
+'''
+Current Problems/Thoughts/Things to do:
+	1) Implement a dictionary taking an ID and returning a miner and then send the correct miner the response to mining.submit in handlPoolLine
+'''
+
+
+
+miners = []
+miningPool = None
+curJon = None
+Target = None
+
+
+class StratumProtocol(protocol.Protocol):
+	def __init__(self, factory):
+		self.factory = factory
+		self.reqID = 4
+
+	def incrementID(self, json_data):
+		json_data['id'] = self.reqID
+		lineNew = json.dumps(json_data, sort_keys=True)
+		self.reqID = self.reqID + 1
+		sep = '['
+		rest = sep + lineNew.split(sep, 1)[1]
+		beg = lineNew.split(sep, 1)[0]
+		rest = re.sub(r'\s+', '', rest)
+		newString = beg + rest + '\n'
+		return newString
+
+	def handleMinerLine(self, line):
+		#BE CAREFUL OF NOT HAVING /n at the end of a string
+		global curJob
+		global target
+		json_data = json.loads(line)
+		print 'sending data to pool'
+		if json_data['method'] == 'mining.submit':
+			miningPool.transport.write(self.incrementID(json_data))
+		else:
+			miningPool.transport.write(line + '\n')
+
+	def handlePoolLine(self, line):
+		for miner in miners:
+			print 'sending data to miner'
+			miner.transport.write(line)
+
+	def connectionMade(self):
+		print '=================================================='
+		print 'Connection Made Function in StratumProtocol'
+		global miningPool
+		if self.factory.__class__ == StratumMinerFactory:
+			miners.append(self)
+			print 'miner connection made'
+		if self.factory.__class__ == StratumPoolFactory:
+			miningPool = self
+			print 'mining pool connection made'
+		print '=================================================='
+
+	def connectionLost(self, reason):
+		print '=================================================='
+		print 'Connection Lost'
+		if self.factory.__class__ == StratumMinerFactory:
+			miners.remove(self)
+			print 'the connection was from a miner'
+		else:
+			print 'the connection was from the pool'
+			print 'trying to reconnect to pool'
+			reactor.connectTCP("us1-zcash.flypool.org", 3333, StratumPoolFactory())
+		print '=================================================='
+
+	def dataReceived(self, data):
+		print '=================================================='
+		print 'Data Received'
+		print data
+		if self.factory.__class__ == StratumMinerFactory:
+			#received data from miner. Send to the pool
+			print ' the data is from the miner'
+			if "\n" in data:
+				for line in data.splitlines():
+					self.handleMinerLine(line)
+			else:
+				self.handleMinerLine(data) 
+
+		elif self.factory.__class__ == StratumPoolFactory:
+			#received data from pool. Send to miner
+			#currently the data is sent to all of the miners, but instead
+			#it should only be sent 
+			print ' the data is from the pool'
+			if "\n" in data:
+				for line in data.splitlines():
+					self.handlePoolLine(line)
+			else:
+				self.handlePoolLine(data) 
+		print '=================================================='
+
+class StratumPoolFactory(protocol.ClientFactory):
+	def __init__(self):
+		print '=================================================='
+		print 'inside StraumFactory init'
+		print '=================================================='
+		return
+
+	def buildProtocol(self, addr):
+		print '=================================================='
+		print 'inside StraumFactory buildProtocol'
+		print '=================================================='
+		return StratumProtocol(self)
+
+	def clientConnectionFailed(self, connector, reason):
+		print '=================================================='
+		print 'inside StraumFactory clientConnectionFailed'
+		print reason
+		print '=================================================='
+
+	def clientConnectionLost(self, connector, reason):
+		print '=================================================='
+		print 'inside StraumFactory clientConnectionLost'
+		print reason
+		print '=================================================='
+
+class StratumMinerFactory(protocol.Factory):
+	def __init__(Self):
+		return
+
+	def buildProtocol(self, addr):
+		return StratumProtocol(self)
+
+	def clientConnectionFailed(self, connector, reason):
+		print '=================================================='
+		print 'inside StraumMinerFactory clientConnectionFailed'
+		print reason
+		print '=================================================='
+
+	def clientConnectionLost(self, connector, reason):
+		print '=================================================='
+		print 'inside StraumMinerFactory clientConnectionLost'
+		print reason
+		print '=================================================='
+
+reactor.connectTCP("us1-zcash.flypool.org", 3333, StratumPoolFactory())
+reactor.listenTCP(3334, StratumMinerFactory())
+reactor.run()
