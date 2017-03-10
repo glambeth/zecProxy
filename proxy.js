9b53fb9cc076fc4b6eee8409202297955f215c9e
george <glambeth1594@gmail.com>
2017-03-10 17:08:09 -0500

    code cleanup + multiple client support

--- proxy.js
+++ proxy.js
@@ -2,5 +2,5 @@ var poolListener = require('./lib/pool_listener.js');
 var config = require('./config.json')
 var minerListener = require('./lib/miner_listener.js')
 
-var startProxy = poolListener.newPoolConnection(config, minerListener.miners);
+poolListener.newPoolConnection(config, minerListener.miners);
 minerListener.createMiningListener(poolListener.poolSocket);
