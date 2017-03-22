0e476e0d51d81a0cf4f7495592dd357b8d1ac7c3
george <glambeth1594@gmail.com>
2017-03-21 23:40:32 -0400

    package.json error

--- proxy.js
+++ proxy.js
@@ -1,6 +1,5 @@
 var poolListener = require('./lib/pool_listener.js');
-var config = require('./config.json')
 var minerListener = require('./lib/miner_listener.js')
 
-poolListener.newPoolConnection(config, minerListener.miners);
+poolListener.newPoolConnection(minerListener.miners);
 minerListener.createMiningListener(poolListener.poolSocket);
